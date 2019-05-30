c1b7fc09ac242f5c6c542b1ae226d2a9d33cd4c1
jerryngton <jerryngtond@gmail.com>
2019-05-30 18:27:58 +0300

    gulpfile for style & serving  mainly

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csscomb = require('gulp-csscomb');
+
+
+gulp.task("style", function() {
+    gulp.src("source/sass/style.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(csscomb())
+        .pipe(gulp.dest("source/css"))
+        .pipe(server.stream());
+});
+
+
+gulp.task("serve", ["style"], function() {
+    server.init({
+        server: "source/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    gulp.watch("source/sass/**/*.scss", ["style"]);
+    gulp.watch("source/css/style.css").on("change", server.reload);
+    gulp.watch("*.html").on("change", server.reload);
+});
