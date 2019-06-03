4ee4de81db38e4e4d7a0b05c6ac4672b8ee27638
jerryngton <jerryngtond@gmail.com>
2019-06-03 19:58:29 +0300

    simple create client script

---
+++ source/js/register/registration.js
@@ -0,0 +1,30 @@
+let createPlan = document.querySelector('.create-button__submit');
+
+createPlan.addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = document.querySelector('.type-field--name');
+    let surname = document.querySelector('.type-field--surname');
+    let email = document.querySelector('.type-field--email');
+
+    fetch('http://localhost:8080/clients/create', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: JSON.stringify({
+            name: name.value,
+            surname: surname.value,
+            email: email.value
+        })
+    })
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((e) => {
+            console.log('bad request:\n' + e);
+        })
+
+});
