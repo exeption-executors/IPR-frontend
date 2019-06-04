60ae687371ad37532f1c5b63599ae70cb72238a0
jerryngton <jerryngtond@gmail.com>
2019-06-04 11:25:22 +0300

    example with get all from backend

---
+++ source/js/test/get-all.js
@@ -0,0 +1,29 @@
+let getClientsButton = document.querySelector('.clients-getter__button');
+let outField = document.querySelector('.clients-getter__out');
+
+getClientsButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetch('http://localhost:8080/clients/list', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*'
+        },
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((clients) => {
+        let buffer = '';
+        clients.forEach((client) => {
+            buffer += `<li>${JSON.stringify(client)}</li>`
+        });
+
+        outField.innerHTML = buffer;
+    })
+    .catch((e) => {
+        console.log('bad request:\n' + e);
+    })
+});
